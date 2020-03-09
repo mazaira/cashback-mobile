7153530fdc56ebda6dfad35208a82f746487ef70
alberto <a.mmazaira@gmail.com>
2020-03-09 18:06:35 +0100

    basic QR

---
+++ README.md
@@ -0,0 +1,11 @@
+# Notes
+
+
+* For the QR code we need:
+
+```
+yarn add react-native-svg
+react-native link react-native-svg
+yarn add react-native-qrcode-svg
+```
+but note that react-native-svg might be buggy https://github.com/expo/expo-cli/issues/1466
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 const HomeScreen = () => {
-  return <View>
-    <Text> Home screen</Text>
+  return <View style={styles.mainBlock}>
+    <Text style={styles.title}> Your code</Text>
+    <QRCode
+      value="http://awesome.link.qr"
+      size={200}
+    />
   </View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainBlock: {
+    borderWidth: 1,
+    borderColor: 'black',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  }
+});
 
 export default HomeScreen;
