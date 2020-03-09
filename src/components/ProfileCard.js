3efcdd55c61103fbba8454c9956399a2ba6c211e
alberto <a.mmazaira@gmail.com>
2020-03-09 21:22:27 +0100

    Adding info to QR

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const ProfileCard = ({account}) => {
-  return <View style={styles.mainBlock}>
-    <Text style={styles.title}> Your Profile</Text>
-    <Text style={styles.title}> Account id: {account.seed}</Text>
-    <QRCode
-      value="http://awesome.link.qr"
-      size={200}
-    />
+  return (
+    <View style={styles.mainBlock}>
+      <MaterialCommunityIcons name='face-profile' />
+      <Text style={styles.title}> Your Profile</Text>
+      <QRCode
+        value={account.seed}
+        size={200}
+      />
   </View>
+  );
 };
 
 const styles = StyleSheet.create({
